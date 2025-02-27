7a8308d91262af5ecaa7154efd8a45aaa5d8ff3b
Devendra Mishra <devendra@mavonic.com>
2025-02-28 01:32:36 +0530

    chore(initial): initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { PlateEditor } from './components/editor/plate-editor'
+
+function App() {
+
+  return (
+    <div className='border-2'>
+      <PlateEditor />
+    </div>
+  )
+}
+
+export default App
